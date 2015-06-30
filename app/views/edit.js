aea65da0f7788da6d580c71b5632bfe791391299
Jason Madsen <jmadsen@instructure.com>
2015-06-30 09:08:25 -0600

    start on editor

--- app/views/edit.js
+++ app/views/edit.js
@@ -1,11 +1,66 @@
 var React = require('react');
+var PlayerStore = require('../stores/player_store');
+var playerStoreListener = require('../mixins/player_store_listener');
 
 var Edit = React.createClass({
 
-  render () {
+  mixins: [playerStoreListener],
+
+  handleSubmit (e) {
+    e.preventDefault();
+    var newState = {
+      displayDuration: this.refs.displayDuration.getDOMNode().value
+    }
+    PlayerStore.setState(newState);
+  },
+
+  renderForm () {
+    return (
+      <form className='pure-form' onSubmit={this.handleSubmit}>
+        <label htmlFor='displayDuration'>
+          Display Duration
+        </label>
+        <input type='text'
+               ref='displayDuration'
+               id='displayDuration'
+               defaultValue={this.state.displayDuration}
+               autoFocus={true}
+               className='pure-input'
+        />
+        <button type='submit' className='pure-button'>Save</button>
+      </form>
+    );
+  },
+
+  renderUrl (url) {
+    return (
+      <div key={url} className='pure-g'>
+        <div className='pure-u-1-12'>
+          <button>d</button>
+        </div>
+        <div className='pure-u-11-12'>
+          {url}
+        </div>
+      </div>
+    );
+  },
+
+  renderUrlManager () {
+    var urls = this.state.urls.map(this.renderUrl);
     return (
       <div>
-        Edit
+        <h2>Urls</h2>
+        {urls}
+      </div>
+    );
+  },
+
+  render () {
+    return (
+      <div className='Edit'>
+        <h1>Framerator Settings</h1>
+        {this.renderForm()}
+        {this.renderUrlManager()}
       </div>
     );
   }
